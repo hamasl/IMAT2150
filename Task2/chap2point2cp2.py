4df33547c6bb7aab4c175698711fc8b34c91255f
Martin Hagen <martin@partins.no>
2021-09-04 15:18:22 +0200

    Completed cp2 in chapter 2.2

--- Task2/chap2point2cp2.py
+++ Task2/chap2point2cp2.py
@@ -1,18 +1,22 @@
 import chap2point2cp1 as cp1
 
 
-def back_substitution(L, U, b):
-    c = [None] * len(L[0])
-    for i in range(len(L) - 1, -1, -1):
-        for j in range(i + 1, len(L)):
-            b[i] -= L[i][j] * c[j]
-        c[i] = b[i] / L[i][i]
+def lc_back_substitution(l, b):
+    c = [None] * len(b)
+    c[0] = b[0]
+    for i in range(1, len(b)):
+        c[i] = b[i]
+        for j in range(0, i):
+            c[i] -= l[i][j] * c[j]
+    return c
 
-    x = [None] * len(U[0])
-    for i in range(len(U) - 1, -1, -1):
-        for j in range(i + 1, len(U)):
-            c[i] -= U[i][j] * x[j]
-        x[i] = c[i] / U[i][i]
+
+def ux_back_substitution(u, b):
+    x = [None] * len(b)
+    for i in range(len(u) - 1, -1, -1):
+        for j in range(i + 1, len(u)):
+            b[i] -= u[i][j] * x[j]
+        x[i] = b[i] / u[i][i]
     return x
 
 
@@ -21,11 +25,10 @@ if __name__ == "__main__":
     a = [[3, 1, 2], [6, 3, 4], [3, 1, 5]]
     ab = [0, 1, 3]
     a_l, a_u = cp1.lu(a)
-    print(back_substitution(a_l, a_u, ab))
+    print(ux_back_substitution(a_u, lc_back_substitution(a_l, ab)))
 
     # Task b
     b = [[4, 2, 0], [4, 4, 2], [2, 2, 3]]
     bb = [2, 4, 6]
     b_l, b_u = cp1.lu(b)
-    print(back_substitution(b_l, b_u, bb))
-    # TODO NB should maybe be 1, -1, 3.0
+    print(ux_back_substitution(b_u, lc_back_substitution(b_l, bb)))
