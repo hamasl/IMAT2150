9b85bd4af56ff260e753fe0d461449f618fc2838
Martin Hagen <martin@partins.no>
2021-09-22 18:50:08 +0200

    Mostly completed cp1 in chapter 6.3

--- Task4/chap6point3cp1.py
+++ Task4/chap6point3cp1.py
@@ -1,49 +1,66 @@
 import math
 
 
-def correct_sol_a(t):
-    return 0.5 * t ** 2 + 1
+def fun_y1(_t, y):
+    return y[0] + y[1]
 
 
-# NOTE know that this is bad practice, but this file will never be used by anyone else than me, and only used for this one exercise
-def fun_a(t, y):
-    y
-    return t
+def correct_sol_y1(t):
+    return math.e ** t * math.cos(t)
 
 
-def correct_sol_b(t):
-    return math.e ** (1 / 3 * t ** 3)
+def fun_y2(_t, y):
+    return y[1] - y[0]
 
 
-def fun_b(t, y):
-    return t ** 2 * y
+def correct_sol_y2(t):
+    return -math.e ** t * math.sin(t)
 
 
+# TODO add correct solution matrix or array
 def eulers_method(fun, t0, w0, h, steps, correct_sol):
     w = [w0]
     t = [t0]
-    error = [0]
+    error = [[0 for _ in range(len(w0))]]
+    print(error)
     for i in range(steps):
         t.append(t[i] + h)
+        wi = []
+        error_i = []
+        for j in range(len(w0)):
+            # TODO configure param w[i]
+            wi.append(w[i][j] + h * fun[j](t[i], w[i]))
+            error_i.append(abs(wi[j] - correct_sol[j](t[i + 1])))
         # TODO maybe add y as a param to fun and correct sol so it can be used for different tasks
-        w.append(w[i] + h * fun(t[i], w[i]))
-        error.append(abs(w[i + 1] - correct_sol(t[i + 1])))
+        w.append(wi)
+        error.append(error_i)
     return w, t, error
 
 
 def print_eulers_res(res):
     w, t, error = res
+    """""
     if not (len(w) == len(t) == len(error)):
+        print("hei")
         return
-    format_string = "{:<8} {:<25} {:<25} {:<15}"
-    print(format_string.format("step", "w", "t", "error"))
-    for i in range(len(w)):
-        print(format_string.format(i, w[i], t[i], error[i]))
+            """""
+
+    format_string = "{:<8} {:<25} {:<25} {:<20}"
+    for j in range(len(w[0])):
+        print(f"\n\nSolutions for y{j + 1}:")
+        print(format_string.format("step", "w", "t", "error"))
+        for i in range(len(w)):
+            print(format_string.format(i, w[i][j], t[i], error[i][j]))
 
 
 if __name__ == "__main__":
-    print("Task a:")
-    print_eulers_res(eulers_method(fun_a, 0, 1, 0.25, 4, correct_sol_a))
+    h = 0.1
+    print(f"Task a with h = {h}:")
+    w0 = [1, 0]
+    funs = [fun_y1, fun_y2]
+    correct_sols = [correct_sol_y1, correct_sol_y2]
+    print_eulers_res(eulers_method(funs, 0, w0, h, 10, correct_sols))
 
-    print("\n\nTask b:")
-    print_eulers_res(eulers_method(fun_b, 0, 1, 0.25, 4, correct_sol_b))
+    h = 0.01
+    print(f"\n\nTask a with h = {h}:")
+    print_eulers_res(eulers_method(funs, 0, w0, h, 100, correct_sols))
