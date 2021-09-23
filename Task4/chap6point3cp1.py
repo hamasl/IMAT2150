927bcc91d50979343d4959cad17e4cc86463e2eb
Martin Hagen <martin@partins.no>
2021-09-23 11:01:10 +0200

    Mostly finished task 6.3.cp1

--- Task4/chap6point3cp1.py
+++ Task4/chap6point3cp1.py
@@ -1,4 +1,6 @@
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def fun_y1(_t, y):
@@ -22,7 +24,6 @@ def eulers_method(fun, t0, w0, h, steps, correct_sol):
     w = [w0]
     t = [t0]
     error = [[0 for _ in range(len(w0))]]
-    print(error)
     for i in range(steps):
         t.append(t[i] + h)
         wi = []
@@ -37,14 +38,7 @@ def eulers_method(fun, t0, w0, h, steps, correct_sol):
     return w, t, error
 
 
-def print_eulers_res(res):
-    w, t, error = res
-    """""
-    if not (len(w) == len(t) == len(error)):
-        print("hei")
-        return
-            """""
-
+def print_eulers_res(w, t, error):
     format_string = "{:<8} {:<25} {:<25} {:<20}"
     for j in range(len(w[0])):
         print(f"\n\nSolutions for y{j + 1}:")
@@ -54,13 +48,37 @@ def print_eulers_res(res):
 
 
 if __name__ == "__main__":
+    # TODO Question ask for is the reduction in error form h = 0.1 to h = 0.01 consistent with the order of euler method
     h = 0.1
     print(f"Task a with h = {h}:")
     w0 = [1, 0]
     funs = [fun_y1, fun_y2]
     correct_sols = [correct_sol_y1, correct_sol_y2]
-    print_eulers_res(eulers_method(funs, 0, w0, h, 10, correct_sols))
+    w, t, error = eulers_method(funs, 0, w0, h, 10, correct_sols)
+    w_np = np.array(w)
+    print_eulers_res(w, t, error)
+
+    fig, axs = plt.subplots(1, 2)
+    axs[0].set_title("y1")
+    axs[0].set_xlabel("x")
+    axs[0].set_ylabel("y")
+    axs[0].plot(t, w_np[:, 0], label=f"Computed solution with h {h}")
+    axs[0].plot(t, [correct_sol_y1(t[i]) for i in range(len(t))], label="Exact solution")
+
+    axs[1].set_title("y2")
+    axs[1].set_xlabel("x")
+    axs[1].set_ylabel("y")
+    axs[1].plot(t, w_np[:, 1], label=f"Computed solution with h {h}")
+    axs[1].plot(t, [correct_sol_y2(t[i]) for i in range(len(t))], label="Exact solution")
 
     h = 0.01
     print(f"\n\nTask a with h = {h}:")
-    print_eulers_res(eulers_method(funs, 0, w0, h, 100, correct_sols))
+    w, t, error = eulers_method(funs, 0, w0, h, 100, correct_sols)
+    w_np = np.array(w)
+    print_eulers_res(w, t, error)
+    axs[0].plot(t, w_np[:, 0], label=f"Computed solution with h {h}")
+    axs[1].plot(t, w_np[:, 1], label=f"Computed solution with h {h}")
+
+    axs[0].legend()
+    axs[1].legend()
+    plt.show()
